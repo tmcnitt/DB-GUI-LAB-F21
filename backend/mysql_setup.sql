00f7f370978e980c80f5c791ecdbf6d25deca002
Shaun Mathew <shaunsanthosh@gmail.com>
2021-10-20 18:53:55 -0500

    Update mysql_setup.sql
    add source table

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,18 +1,25 @@
 -- create database db
 CREATE DATABASE db;
-
 -- use newly create database
 USE db;
-
--- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+CREATE TABLE `users` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(64) NOT NULL,
+    `password` varchar(64) NOT NULL,
+    `user_type` varchar(40) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id` (`id`),
+    UNIQUE KEY `username` (`username`)
 );
+create table tag (id int, tag_content varchar(200));
+alter table tag
+modify column id int not null auto_increment primary key;
 
--- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
-
-
+CREATE TABLE IF NOT EXISTS Source (
+    source_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    source_name VARCHAR(255) NOT NULL,
+    base_url VARCHAR(255),
+    owner_name VARCHAR(255),
+    average_political_bias DECIMAL(5, 2),
+    num_political_votes INT
+);

aee34ccfd766b355ad8531bc09615abb160643d0
Shaun Mathew <shaunsanthosh@gmail.com>
2021-10-27 13:53:54 -0500

    Update mysql_setup.sql

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -2,7 +2,6 @@
 CREATE DATABASE db;
 -- use newly create database
 USE db;
-
 CREATE TABLE `users` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(64) NOT NULL,
@@ -12,14 +11,9 @@ CREATE TABLE `users` (
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `username` (`username`)
 );
-
-create table `tags` (
-    `id` int,
-    `content` varchar(200)
-);
-
-alter table `tags` modify column id int not null auto_increment primary key;
-
+create table `tags` (`id` int, `content` varchar(200));
+alter table `tags`
+modify column id int not null auto_increment primary key;
 CREATE TABLE `articles` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
@@ -28,19 +22,22 @@ CREATE TABLE `articles` (
   `summary` varchar(255) NOT NULL,
   `created_timestamp` DATETIME DEFAULT NOW(),
   `author_name` varchar(255) NOT NULL,
-
   `avg_political_bias` FLOAT,
   `num_political_votes` INT,
-
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 );
-
-CREATE TABLE IF NOT EXISTS sources (
-    source_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    source_name VARCHAR(255) NOT NULL,
-    base_url VARCHAR(255),
-    owner_name VARCHAR(255),
-    average_political_bias DECIMAL(5, 2),
-    num_political_votes INT
+CREATE TABLE IF NOT EXISTS 'sources' (
+  'source_id' INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  'source_name' VARCHAR(255) NOT NULL,
+  'base_url' VARCHAR(255),
+  'owner_name' VARCHAR(255),
+  'average_political_bias' DECIMAL(5, 2),
+  'num_political_votes' INT
+);
+CREATE TABLE IF NOT EXISTS 'tagArticle' (
+  'id' INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  'article_id' INT NOT NULL,
+  'tag_id' INT NOT NULL,
+  'num_likes' INT NOT NULL DEFAULT 0
 );

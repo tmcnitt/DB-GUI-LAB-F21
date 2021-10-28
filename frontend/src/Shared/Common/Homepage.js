86ed428c113a93c7c790ea07b519a6939df3d4c7
ubuntu <myusernamesthis@gmail.com>
2021-10-27 19:37:04 -0500

    before header

--- frontend/src/Shared/Common/Homepage.js
+++ frontend/src/Shared/Common/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "./Card";
 import "./Homepage.css";
 //import Card from "Card";
 export class Homepage extends React.Component {
@@ -7,13 +8,15 @@ export class Homepage extends React.Component {
             <div id="topbar">
                 <img src="https://via.placeholder.com/150"></img>
                 <h1>Political Thingamajig</h1>
+                <div class="clear"></div>
                 <ul id="navbar">
-                    <li><a href="home">Home</a></li>
                     <li><a href="login">Login</a></li>
+                    <li><a href="home">Home</a></li>
                 </ul>
                 <button type="button">Add</button>
+                
             </div>
-            
+            <Card/>
         </>)
     }
 }

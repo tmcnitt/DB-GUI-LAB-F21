627bfd1d3eee560f588a4ee1fadc122048ea6af4
JacksonPaulHeck <jacksonheck1@gmail.com>
2021-11-03 19:59:58 -0500

    Adding a form

--- frontend/src/Shared/Pages/Add.js
+++ frontend/src/Shared/Pages/Add.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Add = () => {
+
     return (
         <div>
         <h1>Add Article</h1>
+        <form id="add-article-form">
+            <label>
+                Name:
+                <input type="text" name="name" />
+            </label>        
+        </form>
+        <Link to="/" class="btn btn-danger">Cancel</Link>
+
+        <Link to="/" class="btn btn-success" type="submit" form="add-article-form" value="Submit">Submit</Link>
+
         </div>
     );
 }

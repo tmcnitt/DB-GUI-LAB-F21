e5ca8920da4c20fc7aa91ba4802931a31d49796f
myusernamesthisreal <51107002+myusernamesthisreal@users.noreply.github.com>
2021-11-10 11:35:17 -0600

    Revert "Login page"

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,47 +1,9 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import axios from 'axios';
+import React from 'react';
 
-export const Login = (props) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [user_type, setUserType] = useState("");
-
-    const handleSubmit = (e) => {
-
-
-    }
-
+export const Login = () => {
     return (
-        <div class="w-75 mx-auto">
-            <div class="border mb-2 mt-5">
-                <h1 class="text-white bg-primary p-3 mb-0">Login</h1>
-                <Form id="login-form" onSubmit={handleSubmit} className="bg-white py-2 mt-0">
-                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="username">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Your Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                        <Form.Control.Feedback type="invalid"> Please enter a username.</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Your Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                        <Form.Control.Feedback type="invalid"> Please enter a password.</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group className="mb-2 ms-3 col-md-4" controlId="password">
-                        <Form.Label>User Type?</Form.Label>
-                        <Form.Select aria-label="Default select example">
-                            <option></option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                        </Form.Select>                    
-                    </Form.Group>
-                </Form>
-            </div>
-
-            <button class="btn btn-success" type="submit" form="add-article-form">Submit</button>
-            <p>Need an account? <Link to="/Signup">Sign Up</Link></p>
+        <div>
+        <h1>Login</h1>
         </div>
     );
 }

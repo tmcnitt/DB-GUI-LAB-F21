990066a0a2dc72fec896406e461e95193afa7b80
myusernamesthis <myusernamesthis@gmail.com>
2021-11-30 18:28:00 -0600

    Most Likely Final

--- frontend/src/Common/SearchBar.jsx
+++ frontend/src/Common/SearchBar.jsx
@@ -34,22 +34,20 @@ export const SearchBar = props => {
     return <div class="w-75 mx-auto">
         <Form id="search-articles">
             <Row>
-                <Form.Group className="mb-2 col-md-4" controlId="title">
-                    <Form.Label>Title</Form.Label>
+                <Form.Group className="mb-2 col-md-6" controlId="title">
                     <Form.Control type="text" maxLength="255" placeholder="Enter Title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </Form.Group>
-                <Form.Group className="mb-2 col-md-4" controlId="authorLastName">
-                    <Form.Label>Author Last Name</Form.Label>
+                <Form.Group className="mb-2 col-md-6" controlId="authorLastName">
                     <Form.Control type="text" maxLength="255" placeholder="Enter Last Name" value={authorLastName} onChange={(e) => setAuthorLastName(e.target.value)} />
                 </Form.Group>
-                <Form.Group className="mb-2 col-4" controlId="authorLastName">
+                {/* <Form.Group className="mb-2 col-4" controlId="authorLastName">
                     <Form.Label>&nbsp;</Form.Label>
                         
                     <button type="button" class="btn btn-primary col-12" onClick={() => changeButtonText()}>{buttonText}</button>
-                </Form.Group>
+                </Form.Group> */}
             </Row>
         </Form>
-        <button class="btn btn-success col-12" onClick={() => props.onSearch({ title, authorLastName })} form="search-form">Search</button>
+        <button class="btn btn-success col-md-12" onClick={() => props.onSearch({ title, authorLastName })} form="search-form">Search</button>
     </div>
 
 };
